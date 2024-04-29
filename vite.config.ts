397638e8ab0037ffcb0929decad80fed67642eeb
Shinya Hara <rabby.coffee.c5@gmail.com>
2024-04-29 18:19:32 +0900

    👷 github pagesにデプロイするgithub actionの定義

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,5 @@
+import { copyFileSync } from "node:fs";
+import { join } from "node:path";
 import { vitePlugin as remix } from "@remix-run/dev";
 import { installGlobals } from "@remix-run/node";
 import { defineConfig } from "vite";
@@ -6,9 +8,19 @@ import tsconfigPaths from "vite-tsconfig-paths";
 installGlobals();
 
 export default defineConfig({
+  base: "/remix-todo-app-spa/",
   plugins: [
     remix({
       ssr: false,
+      basename: "/remix-todo-app-spa/",
+      buildEnd(args) {
+        if (!args.viteConfig.isProduction) return;
+        const buildPath = args.viteConfig.build.outDir;
+        copyFileSync(
+          join(buildPath, "index.html"),
+          join(buildPath, "404.html")
+        );
+      },
     }),
     tsconfigPaths(),
   ],
